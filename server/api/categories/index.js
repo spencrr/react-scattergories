82280f85c08c8d96c2cd394cc931f9a2ae7706c9
spencrr <23708360+spencrr@users.noreply.github.com>
2020-04-15 01:31:31 -0500

    Scattergories created with create-react-app

---
+++ server/api/categories/index.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+
+const createCategories = require("../../utils/createCategories")(10);
+const getLetter = require("../../utils/getLetter");
+
+const refresh = () => ({
+  categories: createCategories(),
+  letter: getLetter(),
+});
+
+router.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+router.get("/", (req, res) => {
+  res.send(refresh());
+});
+
+let db = {};
+
+const getSession = ({ update = false } = {}) => (req, res) => {
+  const { sessionId } = req.params;
+  if (update || !db[sessionId]) {
+    db[sessionId] = refresh();
+  }
+  res.send(db[sessionId]);
+};
+
+router.get("/:sessionId", getSession());
+
+router.post("/:sessionId", getSession({ update: true }));
+
+module.exports = {
+  api: router,
+  updateCategories(sessionId) {
+    db[sessionId] = refresh();
+  },
+};
---
+++ src/components/Score.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Score({ score }) {
+  return <h1>{score}</h1>;
+}
