095f9c21727ed0cb965a3614b11cacf98ace6d65
spencrr <23708360+spencrr@users.noreply.github.com>
2021-01-20 21:56:29 -0600

    split client and server into separate folders and packages

--- server/index.js
+++ server/index.js
@@ -12,6 +12,6 @@ socketHandler({ http, updateCategories });
 const PORT = process.env.PORT || 5000;
 http.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static(path.join(__dirname, "..", "build")));
 
 app.use("/api/categories", categoryAPI);
